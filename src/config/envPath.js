2865f56356b2dfd56f9f2bb18732e57ba452972b
Gonzalo Gramajo <gramajogonzalo@gmail.com>
2022-09-27 22:58:20 -0300

    Listo

--- src/config/envPath.js
+++ src/config/envPath.js
@@ -1,19 +1,12 @@
-const argv = require('yargs/yargs')(process.argv.slice(2)).argv
-const dotenv = require('dotenv');
-const path = require('path');
-
-/* dotenv.config({
-    path: path.resolve(process.cwd(), argv.NODE_ENV + '.env')
-}) */
+const malditoDotenv = require('../../maldito-dotenv')
 
 module.exports = {
-    NODE_ENV: process.env.NODE_ENV || 'development',
-    //HOST: process.env.HOST || 'localhost',
-    //PORT: process.env.PORT || 8080,
-    MONGODB_URI: process.env.MONGODB_URI,
-    SESSION_TIME: process.env.SESSION_TIME || 20000,
-    TIPO_PERSISTENCIA: process.env.TIPO_PERSISTENCIA || 'MONGO',
-    GMAIL_MAIL: process.env.GMAIL_MAIL,
-    GMAIL_PASS: process.env.GMAIL_PASS
+    NODE_ENV: malditoDotenv.NODE_ENV,
+    HOST: malditoDotenv.HOST,
+    PORT: malditoDotenv.PORT,
+    MONGODB_URI: malditoDotenv.MONGODB_URI,
+    SESSION_TIME: malditoDotenv.SESSION_TIME,
+    TIPO_PERSISTENCIA: malditoDotenv.TIPO_PERSISTENCIA,
+    GMAIL_MAIL: malditoDotenv.GMAIL_MAIL,
+    GMAIL_PASS: malditoDotenv.GMAIL_PASS
 }
-

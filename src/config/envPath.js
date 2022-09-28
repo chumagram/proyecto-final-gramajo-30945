e4175a6aaa477b9ccb83cf6389b0a5acf6b270b2
Gonzalo Gramajo <gramajogonzalo@gmail.com>
2022-09-27 21:16:33 -0300

    make it better

--- src/config/envPath.js
+++ src/config/envPath.js
@@ -2,14 +2,14 @@ const argv = require('yargs/yargs')(process.argv.slice(2)).argv
 const dotenv = require('dotenv');
 const path = require('path');
 
-dotenv.config({
+/* dotenv.config({
     path: path.resolve(process.cwd(), argv.NODE_ENV + '.env')
-})
+}) */
 
 module.exports = {
     NODE_ENV: process.env.NODE_ENV || 'development',
-    HOST: process.env.HOST || 'localhost',
-    PORT: process.env.PORT || 8080,
+    //HOST: process.env.HOST || 'localhost',
+    //PORT: process.env.PORT || 8080,
     MONGODB_URI: process.env.MONGODB_URI,
     SESSION_TIME: process.env.SESSION_TIME || 20000,
     TIPO_PERSISTENCIA: process.env.TIPO_PERSISTENCIA || 'MONGO',
